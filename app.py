aec929c276863b88b647909bede256997a903a7f
luddthree~ <luddetv@gmail.com>
2023-08-30 09:54:41 +0200

    app

--- app.py
+++ app.py
@@ -40,6 +40,22 @@ def about():
         return render_template('about1.html')
     else:
         return render_template('about.html')
+
+
+@app.route('/app')
+def logon():
+    if request.headers.get('User-Agent') == 'WebView':
+        return render_template('index_app.html')
+    else:
+        return redirect(url_for('index.html'))  # Redirect to login page if not accessed from web view
+
+@app.route('/abapp')
+def about():
+    if request.headers.get('User-Agent') == 'WebView':
+        return render_template('about_app.html')
+    else:
+        return redirect(url_for('index.html'))  # Redirect to login page if not accessed from web view
+
     
 
 @app.route('/enternew')
