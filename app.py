ec3ae2e74320b8306ba6fd26be28770f01d03027
luddthree~ <luddetv@gmail.com>
2023-08-31 14:20:08 +0200

    admin fixes and password checking

--- app.py
+++ app.py
@@ -67,12 +67,15 @@ def addrec():
             name=request.form['name']
             pin=request.form['pin']
 
-            with sql.connect(databasePath) as con:
-                cur = con.cursor()
-                cur.execute("INSERT INTO login (name,pin) VALUES (?,?)",(name,pin))
-                con.commit()
-                session['username'] = name
-                msg = "Welcome to Pisscounter " + name + "!"
+            if check_duplicates(name):
+                msg = "Username already taken"
+            else:
+                with sql.connect(databasePath) as con:
+                    cur = con.cursor()
+                    cur.execute("INSERT INTO login (name,pin) VALUES (?,?)",(name,pin))
+                    con.commit()
+                    session['username'] = name
+                    msg = "Welcome to Pisscounter " + name + "!"
                 
         except BaseException as e:
             con.rollback()
@@ -90,12 +93,49 @@ def list():
     rows = cur.fetchall()
     return render_template('list.html',rows=rows)
 
-@app.route('/login', methods=['POST', 'GET'])
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     if request.method == 'POST':
+#         try:
+#             name=request.form['name']
+#             pin=request.form['pin']
+#             with sql.connect(databasePath) as con:
+#                 cur = con.cursor()
+#                 try:
+#                     sqlite_insert_query = """SELECT * from login where
+#                     name='""" + name + """' and pin='""" + pin + """'"""
+#                     cur.execute(sqlite_insert_query)
+#                     records = cur.fetchall()
+#                     if (len(records) >= 1):
+#                         session['username'] = name
+#                         msg ="Welcome back, " + name +"!"
+#                     else:
+#                         msg = "Wrong username or password"
+#                 except:
+#                     msg = "error"
+#         except:
+#             msg="error in insert operation" + " " + msg
+#         finally:
+#             return render_template("result.html", msg=msg)
+#             con.close()
+
+def check_duplicates(name):
+    conn = sql.connect(databasePath)
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM login")
+    rows = cursor.fetchall()
+    usernames = [row[0] for row in rows]
+    if name in usernames:
+        return True
+    return False
+
+
+@app.route('/logon', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         try:
-            name=request.form['name']
-            pin=request.form['pin']
+            name = request.form['name']
+            pin = request.form['pin']
             with sql.connect(databasePath) as con:
                 cur = con.cursor()
                 try:
@@ -103,19 +143,39 @@ def login():
                     name='""" + name + """' and pin='""" + pin + """'"""
                     cur.execute(sqlite_insert_query)
                     records = cur.fetchall()
-                    if (len(records) >= 1):
+                    if len(records) >= 1:
                         session['username'] = name
-                        msg ="Welcome back, " + name +"!"
+                        if name == 'admin':
+                           msg = "Admin Login successful"
+                        else:
+                            msg = "Welcome back, " + name +"!"
                     else:
                         msg = "Wrong username or password"
                 except:
-                    msg = "error"
+                    msg = "Error executing query"
         except:
-            msg="error in insert operation" + " " + msg
+            msg = "Error in form submission"
         finally:
-            return render_template("result.html", msg=msg)
-            con.close()
+            if msg == "Admin Login successful":
+                conn1 = sql.connect(databasePath)
+                conn1.row_factory = sql.Row
+                cur1 = conn1.cursor()
+                cur1.execute("SELECT * FROM login")
+                rows1 = cur1.fetchall()
+                conn1.close()
 
+                # Connect to the second database
+                conn2 = sql.connect(databasePath)
+                conn2.row_factory = sql.Row
+                cur2 = conn2.cursor()
+                cur2.execute("SELECT * FROM markers")
+                rows2 = cur2.fetchall()
+                conn2.close()
+                return render_template('admin_page.html',rows1=rows1, rows2=rows2)
+            else:
+                return render_template("result.html", msg=msg)
+    
+    return render_template('login.html')
 
 @app.route('/map')
 def root():
@@ -162,6 +222,93 @@ def remove_marker():
 
     return jsonify({'message': 'Marker removed successfully'})
 
+
+
+@app.route('/delete/<string:record_id>', methods=['POST'])
+def delete_record(record_id):
+    conn = sql.connect(databasePath)
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM login WHERE name = ?", (record_id,))
+
+    conn.commit()
+    conn.close()
+    conn1 = sql.connect(databasePath)
+    conn1.row_factory = sql.Row
+    cur1 = conn1.cursor()
+    cur1.execute("SELECT * FROM login")
+    rows1 = cur1.fetchall()
+    conn1.close()
+
+    # Connect to the second database
+    conn2 = sql.connect(databasePath)
+    conn2.row_factory = sql.Row
+    cur2 = conn2.cursor()
+    cur2.execute("SELECT * FROM markers")
+    rows2 = cur2.fetchall()
+    conn2.close()
+    return render_template("admin_page.html", rows1=rows1, rows2=rows2)
+
+@app.route('/delete1/<string:record_id>', methods=['POST'])
+def delete1_record(record_id):
+    conn = sql.connect(databasePath)
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM markers WHERE lat = ?", (record_id,))
+
+    conn.commit()
+    conn.close()
+
+    conn1 = sql.connect(databasePath)
+    conn1.row_factory = sql.Row
+    cur1 = conn1.cursor()
+    cur1.execute("SELECT * FROM login")
+    rows1 = cur1.fetchall()
+    conn1.close()
+
+    # Connect to the second database
+    conn2 = sql.connect(databasePath)
+    conn2.row_factory = sql.Row
+    cur2 = conn2.cursor()
+    cur2.execute("SELECT * FROM markers")
+    rows2 = cur2.fetchall()
+    conn2.close()
+    return render_template("admin_page.html", rows1=rows1, rows2=rows2)
+
+
+@app.route('/add_data', methods=['POST'])
+def add_data():
+    if request.method == 'POST':
+        # Retrieve form data
+        name = request.form['name']
+        pin = request.form['pin']
+
+        # Insert data into database
+        conn = sql.connect(databasePath)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO login (name, pin) VALUES (?, ?)", (name, pin))
+        conn.commit()
+        conn.close()
+
+        # Redirect to homepage
+        conn1 = sql.connect(databasePath)
+        conn1.row_factory = sql.Row
+        cur1 = conn1.cursor()
+        cur1.execute("SELECT * FROM login")
+        rows1 = cur1.fetchall()
+        conn1.close()
+
+        # Connect to the second database
+        conn2 = sql.connect(databasePath)
+        conn2.row_factory = sql.Row
+        cur2 = conn2.cursor()
+        cur2.execute("SELECT * FROM markers")
+        rows2 = cur2.fetchall()
+        conn2.close()
+        return render_template("admin_page.html", rows1=rows1, rows2=rows2)
+
+    # Handle GET request or other cases
+    return render_template("admin_page.html", rows1=rows1, rows2=rows2)
 
 
 if __name__ == "__main__":
