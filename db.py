f5b960ca1c394787c0e0e88ac34df7e2e3753c97
luddthree~ <luddetv@gmail.com>
2023-09-12 14:54:34 +0200

    meta

--- db.py
+++ db.py
@@ -1,9 +1,17 @@
-import sqlite3
+import mysql.connector
 
-conn = sqlite3.connect("database.db")
-print("opened db with sucess")
+# Establish a connection to the MySQL server
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="Password123#@!",
+)
 
-conn.execute("CREATE TABLE login (name TEXT, pin TEXT)")
-print("table created sucessfully")
+# Create a cursor object to execute SQL queries
+my_cursor = mydb.cursor()
 
-conn.close()
+# Create a new database
+my_cursor.execute("CREATE DATABASE mydatabase")  
+my_cursor.execute("SHOW DATABASES")
+for db in my_cursor:
+    print(db)
